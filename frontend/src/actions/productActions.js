1ea38c72082afcdd465cd95a4ad0ba012d11cbe3
Burhan <2019cs626@student.uet.edu.pk>
2021-09-30 16:23:19 +0500

    implemented Redux on the Product Details Page

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants'
+import { PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST, PRODUCT_DETAIL_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants'
 
 // These are actually the action creators
 export const listProducts = () => async (dispatch) => { // Redux thunk allows us to make another function within a function for async porposes
@@ -15,6 +15,25 @@ export const listProducts = () => async (dispatch) => { // Redux thunk allows us
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })
+    }
+}
+export const listProductDetails = (id) => async (dispatch) => { // Redux thunk allows us to make another function within a function for async porposes
+    try {
+        dispatch({ type: PRODUCT_DETAIL_REQUEST })
+
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAIL_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAIL_FAIL,
             payload: error.response && error.response.data.message
                 ? error.response.data.message
                 : error.message
