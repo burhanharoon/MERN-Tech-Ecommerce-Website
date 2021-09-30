81272c60ef2330e07cb7c744f3ee5c29a47c4f0a
Burhan <2019cs626@student.uet.edu.pk>
2021-09-30 14:20:54 +0500

    Added Redux in the project

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants'
+
+// These are actually the action creators
+export const listProducts = () => async (dispatch) => { // Redux thunk allows us to make another function within a function for async porposes
+    try {
+        dispatch({ type: PRODUCT_LIST_REQUEST })
+
+        const { data } = await axios.get('/api/products')
+
+        dispatch({
+            type: PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
-import { createStore, combineReducers, applyMiddlewareF, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
+
+const reducer = combineReducers({
+    productList: productListReducer
+})
 
 const initialState = {}
 
-const reducer = () => { }
 
 const middleware = [thunk]
 
