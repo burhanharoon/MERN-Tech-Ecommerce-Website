ea2fad41ff01960d3c614fcaa68eba601d1cfb87
Burhan <2019cs626@student.uet.edu.pk>
2021-10-15 15:21:34 +0500

    Added auth middleware for accessing private routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,12 +3,10 @@ import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
 // @desc    Auth user
-// @route   GET /api/users/login
+// @route   POST /api/users/login
 // @access  PUBLIC
 
 const authUser = asyncHandler(async (req, res) => {
-    // In order to use res.body data we would have to add 
-
 
     const { email, password } = req.body
 
@@ -30,6 +28,30 @@ const authUser = asyncHandler(async (req, res) => {
 
 })
 
+
+// @desc    GET User Profile
+// @route   GET /api/users/profile
+// @access  PRIVATE
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+
+    }
+
+})
+
 export {
-    authUser
+    authUser,
+    getUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
+import protect from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
@@ -7,5 +8,6 @@ router.get('/', (req, res) => {
     res.send("Users")
 })
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
